c9e19feba09a9997f0208b6e188776720d2cc707
Thelordgreat <greatokafor15@gmail.com>
2022-11-04 23:57:08 +0100

    ädded validation to form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,40 +1,52 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+    const [messageField, setMessageField] = useState('')
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (messageField.length == 0) {
+            setError(true)
+        }
+    }
+
     return (
         <div className='form_container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Contact Me</h2>
                 <p className='heading'>Hi there, contact me to ask me about anything you have in mind.</p>
 
                 <div className='name_row'>
                     <div>
                         <label>First Name:</label>
-                        <input type="name" placeholder='Enter your first name'/>
+                        <input type="name" placeholder='Enter your first name' id='first_name' required />
                     </div>
                     <div>
                         <label>Last Name:</label>
-                        <input type="name"placeholder='Enter your last name'/>
+                        <input type="name" placeholder='Enter your last name' id='last_name' required />
                     </div>
                 </div>
 
                 <div className='row'>
                     <label>Email:</label>
-                    <input type="email" placeholder='yourname@email.com'/>
+                    <input type="email" placeholder='yourname@email.com' id='email' required />
                 </div>
 
                 <div className='row'>
                     <label>Message:</label>
-                    <input type="text" className='message_field' placeholder=""/>
+                    <input type="text" className='message_field' placeholder="" id='message' onChange={e => setMessageField(e.target.value)} />
+                    {error&&messageField.length<=0? <span>Please enter a message</span> : ""}
                 </div>
 
                 <div className='terms'>
-                    <input type="checkbox" />
-                    <p>You agree to providing your data to name who may contact you.</p>
+                    <input type="checkbox" required />
+                    <p>You agree to providing your data to Great who may contact you.</p>
                 </div>
 
                 <div>
-                    <button>Send message</button>
+                    <button id='btn_submit'>Send message</button>
                 </div>
             </form>
         </div>
